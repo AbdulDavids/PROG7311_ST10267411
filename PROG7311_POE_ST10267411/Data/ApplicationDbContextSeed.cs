2150b1ccda98365e402cd9227bd4dd4de3aaa54c
Abdul Baari Davids <abdulbaaridavids04@gmail.com>
2025-05-14 20:51:07 +0200

    tests fix

--- PROG7311_POE_ST10267411/Data/ApplicationDbContextSeed.cs
+++ PROG7311_POE_ST10267411/Data/ApplicationDbContextSeed.cs
@@ -21,11 +21,11 @@ namespace PROG7311_POE_ST10267411.Data
                 }
             }
 
-            // Create employee user
+            // Create employee user - Elon for energy company
             var employeeUser = new ApplicationUser
             {
-                UserName = "employee@demo.local",
-                Email = "employee@demo.local",
+                UserName = "elon@gmail.com",
+                Email = "elon@gmail.com",
                 EmailConfirmed = true
             };
 
@@ -35,11 +35,11 @@ namespace PROG7311_POE_ST10267411.Data
                 await userManager.AddToRoleAsync(employeeUser, "Employee");
             }
 
-            // Create demo farmer users
+            // Create demo farmer users - FAANG CEOs as farmers
             var farmer1 = new ApplicationUser
             {
-                UserName = "farmer1@demo.local",
-                Email = "farmer1@demo.local",
+                UserName = "sundar@gmail.com",
+                Email = "sundar@gmail.com",
                 EmailConfirmed = true
             };
 
@@ -51,8 +51,8 @@ namespace PROG7311_POE_ST10267411.Data
 
             var farmer2 = new ApplicationUser
             {
-                UserName = "farmer2@demo.local",
-                Email = "farmer2@demo.local",
+                UserName = "jeff@gmail.com",
+                Email = "jeff@gmail.com",
                 EmailConfirmed = true
             };
 
@@ -60,6 +60,32 @@ namespace PROG7311_POE_ST10267411.Data
             {
                 await userManager.CreateAsync(farmer2, "Password123!");
                 await userManager.AddToRoleAsync(farmer2, "Farmer");
+            }
+            
+            var farmer3 = new ApplicationUser
+            {
+                UserName = "tim@gmail.com",
+                Email = "tim@gmail.com",
+                EmailConfirmed = true
+            };
+
+            if (await userManager.FindByEmailAsync(farmer3.Email) == null)
+            {
+                await userManager.CreateAsync(farmer3, "Password123!");
+                await userManager.AddToRoleAsync(farmer3, "Farmer");
+            }
+            
+            var farmer4 = new ApplicationUser
+            {
+                UserName = "mark@gmail.com",
+                Email = "mark@gmail.com",
+                EmailConfirmed = true
+            };
+
+            if (await userManager.FindByEmailAsync(farmer4.Email) == null)
+            {
+                await userManager.CreateAsync(farmer4, "Password123!");
+                await userManager.AddToRoleAsync(farmer4, "Farmer");
             }
         }
 
@@ -69,84 +95,172 @@ namespace PROG7311_POE_ST10267411.Data
             if (!context.Farmers.Any())
             {
                 // Get the demo farmer users
-                var farmer1User = await userManager.FindByEmailAsync("farmer1@demo.local");
-                var farmer2User = await userManager.FindByEmailAsync("farmer2@demo.local");
+                var farmer1User = await userManager.FindByEmailAsync("sundar@gmail.com");
+                var farmer2User = await userManager.FindByEmailAsync("jeff@gmail.com");
+                var farmer3User = await userManager.FindByEmailAsync("tim@gmail.com");
+                var farmer4User = await userManager.FindByEmailAsync("mark@gmail.com");
 
-                // Create sample farmers
+                // Create sample farmers - FAANG CEOs
                 var farmer1 = new Farmer
                 {
-                    Name = "John Smith",
-                    Email = "farmer1@demo.local",
-                    Phone = "081-555-0123",
+                    Name = "Sundar Pichai",
+                    Email = "sundar@gmail.com",
+                    Phone = "650-253-0000",
                     UserId = farmer1User?.Id
                 };
 
                 var farmer2 = new Farmer
                 {
-                    Name = "Mary Johnson",
-                    Email = "farmer2@demo.local",
-                    Phone = "082-555-0456",
+                    Name = "Jeff Bezos",
+                    Email = "jeff@gmail.com",
+                    Phone = "206-266-1000",
                     UserId = farmer2User?.Id
+                };
+                
+                var farmer3 = new Farmer
+                {
+                    Name = "Tim Cook",
+                    Email = "tim@gmail.com",
+                    Phone = "408-996-1010",
+                    UserId = farmer3User?.Id
+                };
+                
+                var farmer4 = new Farmer
+                {
+                    Name = "Mark Zuckerberg",
+                    Email = "mark@gmail.com",
+                    Phone = "650-853-1300",
+                    UserId = farmer4User?.Id
                 };
 
                 context.Farmers.Add(farmer1);
                 context.Farmers.Add(farmer2);
+                context.Farmers.Add(farmer3);
+                context.Farmers.Add(farmer4);
                 await context.SaveChangesAsync();
 
-                // Create sample products for farmer 1
+                // Create sample products for Sundar Pichai
                 var productsForFarmer1 = new List<Product>
                 {
                     new Product
                     {
-                        Name = "Organic Corn",
+                        Name = "Yellow Corn",
                         Category = "Grains",
-                        ProductionDate = DateTime.Now.AddDays(-30),
+                        ProductionDate = DateTime.Now.AddDays(-44),
                         FarmerId = farmer1.Id
                     },
                     new Product
                     {
-                        Name = "Sweet Potatoes",
-                        Category = "Vegetables",
-                        ProductionDate = DateTime.Now.AddDays(-15),
+                        Name = "Organic Soybeans",
+                        Category = "Legumes",
+                        ProductionDate = DateTime.Now.AddDays(-77),
                         FarmerId = farmer1.Id
                     },
                     new Product
                     {
-                        Name = "Alfalfa",
-                        Category = "Biomass",
-                        ProductionDate = DateTime.Now.AddDays(-7),
+                        Name = "Winter Wheat",
+                        Category = "Grains",
+                        ProductionDate = DateTime.Now.AddDays(-25),
                         FarmerId = farmer1.Id
                     }
                 };
 
-                // Create sample products for farmer 2
+                // Create sample products for Jeff Bezos
                 var productsForFarmer2 = new List<Product>
                 {
                     new Product
                     {
-                        Name = "Sugarcane",
-                        Category = "Sugar Crops",
-                        ProductionDate = DateTime.Now.AddDays(-45),
+                        Name = "Fresh Carrots",
+                        Category = "Vegetables",
+                        ProductionDate = DateTime.Now.AddDays(-33),
                         FarmerId = farmer2.Id
                     },
                     new Product
                     {
-                        Name = "Soybeans",
-                        Category = "Legumes",
-                        ProductionDate = DateTime.Now.AddDays(-20),
+                        Name = "Sugarcane",
+                        Category = "Sugar Crops",
+                        ProductionDate = DateTime.Now.AddDays(-18),
                         FarmerId = farmer2.Id
                     },
+                    new Product
+                    {
+                        Name = "Biofuel Corn",
+                        Category = "Biomass",
+                        ProductionDate = DateTime.Now.AddDays(-11),
+                        FarmerId = farmer2.Id
+                    },
+                    new Product
+                    {
+                        Name = "Sweet Corn",
+                        Category = "Grains",
+                        ProductionDate = DateTime.Now.AddDays(-5),
+                        FarmerId = farmer2.Id
+                    }
+                };
+                
+                // Create sample products for Tim Cook
+                var productsForFarmer3 = new List<Product>
+                {
+                    new Product
+                    {
+                        Name = "Oranges",
+                        Category = "Fruits",
+                        ProductionDate = DateTime.Now.AddDays(-55),
+                        FarmerId = farmer3.Id
+                    },
+                    new Product
+                    {
+                        Name = "Roma Tomatoes",
+                        Category = "Vegetables",
+                        ProductionDate = DateTime.Now.AddDays(-37),
+                        FarmerId = farmer3.Id
+                    },
+                    new Product
+                    {
+                        Name = "Red Beets",
+                        Category = "Vegetables",
+                        ProductionDate = DateTime.Now.AddDays(-14),
+                        FarmerId = farmer3.Id
+                    }
+                };
+                
+                // Create sample products for Mark Zuckerberg
+                var productsForFarmer4 = new List<Product>
+                {
                     new Product
                     {
                         Name = "Switchgrass",
                         Category = "Biomass",
-                        ProductionDate = DateTime.Now.AddDays(-10),
-                        FarmerId = farmer2.Id
+                        ProductionDate = DateTime.Now.AddDays(-41),
+                        FarmerId = farmer4.Id
+                    },
+                    new Product
+                    {
+                        Name = "Red Wheat",
+                        Category = "Grains",
+                        ProductionDate = DateTime.Now.AddDays(-22),
+                        FarmerId = farmer4.Id
+                    },
+                    new Product
+                    {
+                        Name = "Industrial Hemp",
+                        Category = "Biomass",
+                        ProductionDate = DateTime.Now.AddDays(-8),
+                        FarmerId = farmer4.Id
+                    },
+                    new Product
+                    {
+                        Name = "Millet",
+                        Category = "Grains",
+                        ProductionDate = DateTime.Now.AddDays(-3),
+                        FarmerId = farmer4.Id
                     }
                 };
 
                 context.Products.AddRange(productsForFarmer1);
                 context.Products.AddRange(productsForFarmer2);
+                context.Products.AddRange(productsForFarmer3);
+                context.Products.AddRange(productsForFarmer4);
                 await context.SaveChangesAsync();
             }
         }
